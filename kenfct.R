97ff57fc7770cf900ecb1835e56cdd8e5e9a5394
LuciaSegovia <lsegoviarevilla@gmail.com>
2021-08-18 17:06:04 +0100

    finalized script for ken

--- kenfct.R
+++ kenfct.R
@@ -139,24 +139,125 @@ kenfct %>%
   mutate(FOLAC = (FOLDFE-FOLFD)/1.7) %>% select(code, FOLAC) %>% 
   filter(!code %in% folac, FOLAC> 0) %>% arrange(FOLAC) %>% knitr::kable()
 
-
 #Detecting FOLAC (folic acid used in fortified food)
+#15065 - Fortified. Misreported in excel but reported in pdf
+#15019 - It seems fortified, although it's not reported as such. 
 
-#MISSING 2 (17 vs 19)
-#*(15065) includes 52 mcg of FOLAC
+###=======================DONT RUN==========================================####
+#   kenfct %>% filter(str_detect(FOLDFE, '[*]')) %>% pull(code, FOLDFE)        
+#   
+#   
+#   
+#   kenfct %>% select(-fooditem) %>%  
+#     str_subset(., "[*]") 
+#   
+#   #Use function to remove bracket and create a new variable 
+#   #to calculate the fortification from FOLDFE
+#   
+#   kenfct %>%  
+#     mutate(FOLAC = ifelse( 
+#    str_detect(FOLDFE, '[*]'), str_extract(FOLDFE, "[:digit:]+"), FOLDFE)) %>% 
+#      mutate_at(c("FOLAC", "FOLFD"), as.numeric) %>% 
+#      mutate(FOLAC = (FOLAC-FOLFD)/1.7)
+###############################################################################   
 
-kenfct %>% filter(str_detect(FOLDFE, '[*]')) %>% pull(code, FOLDFE) 
+##3) MAPS type format
+
+wafct.genus <- read.csv(here::here('metadata', 'MAPS_WAFCT_standard-list.csv'))
+
+var.name <- read.csv(here::here("fct-variable-names.csv")) %>% 
+  select(Column.Name) %>% pull()
+
+source("dictionary.R")
+
+## Adding GENuS code (1)
+
+ken.genus %>% count(ref_fctcode) %>% arrange(desc(n))
+#       13031 2
+#        15026 2
+
+kenfct %>% filter(code == "6026") %>% pull(fooditem)
+dictionary %>% filter(ID_3 == "22230.01.01")
+
+ken_genus <- tribble(
+  ~ref_fctcode,   ~ID_3, ~confidence,
+  "1004",   "F0022.03",    "h",
+  "6001",   "22241.01.01", "h",
+  "12004",  "F0665.01",    "m", 
+  "12003",  "23912.02.01", "h", 
+  "4016",   "1232.01"  ,   "h",
+  "11001",  "2910.01"  ,   "h",
+  "10010",  "1379.9.01" ,  "h", 
+  "13027",  "1699.02"   ,  "m", #salt, ionized vs salt
+  "15026",  "F0022.05"  ,  "h",
+  "1031",   "23710.01"  ,  "h", 
+  "13028",  "1699.03"   ,  "h",
+  "11003",  "23520.01"  ,  "h",
+  "12005",  "23914.02"  ,  "m",
+  "13031",  "21399.01.01" ,"m",
+  "12008",  "24212.02.01" ,"h",
+  "6026",   "22230.01.01" ,"h")
+
+ken_genus <- ken_genus %>% left_join(., dictionary)
 
 
+#Rename variables according to MAPS-standards
 
-kenfct %>% select(-fooditem) %>%  
-  str_subset(., "[*]") 
+MAPS_ken <- kenfct %>% 
+  left_join(., ken_genus, by = c("code" = "ref_fctcode")) %>%   
+rename(
+  original_food_id = "code",
+  original_food_name = "fooditem",
+  food_genus_id = "ID_3",
+  food_genus_description = "FoodName_3",
+  food_group = "FoodName_0",
+  food_subgroup = "FoodName_1", 
+  food_genus_confidence = "confidence",
+  fct_name = "FCT",
+  data_reference_original_id = "biblio_id",
+  moisture_in_g = "WATER",
+  energy_in_kcal = "ENERC1",
+  energy_in_kj = "ENERC2",
+  totalprotein_in_g = "PROTCNT",
+  totalfats_in_g = "FAT",
+  saturatedfa_in_g = "FASAT", 
+  monounsaturatedfa_in_g = "FAMS", 
+  polyunsaturatedfa_in_g = "FAPU", 
+  cholesterol_in_mg = "CHOLE",
+  carbohydrates_in_g = "CHOAVLDF", 
+  fibre_in_g = "FIBTG", 
+  ash_in_g = "ASH",
+  ca_in_mg = "CA", 
+  fe_in_mg = "FE",
+  mg_in_mg = "MG",
+  p_in_mg = "P",
+  k_in_mg = "K",
+  na_in_mg = "NA.", 
+  zn_in_mg = "ZN",
+  se_in_mcg = "SE",
+  vitamina_in_rae_in_mcg = "VITA_RAE", 
+  thiamin_in_mg = "THIA",
+  riboflavin_in_mg = "RIBF", 
+  niacin_in_mg = "NIA", 
+  folate_in_mcg = "FOLFD",
+  vitaminb12_in_mcg = "VITB12",
+  vitaminc_in_mg = "VITC",
+  phyticacid_in_mg = "PHYTCPPD") %>% 
+  mutate(
+  nitrogen_in_g = "NA", 
+  cu_in_mg = "NA",
+  mn_in_mcg = "NA",
+  i_in_mcg = "NA",
+  vitaminb6_in_mg = "NA",
+  pantothenate_in_mg = "NA",
+  biotin_in_mcg = "NA",
+  vitamind_in_mcg = "NA",
+  vitamine_in_mg = "NA",
+  folicacid_in_mcg = "NA") %>% select(var.name)
 
-#Use function to remove bracket and create a new variable 
-#to calculate the fortification from FOLDFE
 
-kenfct %>%  
-  mutate(FOLAC = ifelse( 
-    str_detect(FOLDFE, '[*]'), str_extract(FOLDFE, "[:digit:]+"), FOLDFE)) %>% 
-   mutate_at(c("FOLAC", "FOLFD"), as.numeric) %>% 
-   mutate(FOLAC = (FOLAC-FOLFD)/1.7)
+MAPS_ken %>% head()
+
+readr::write_excel_csv(., 
+                       here::here('output', 'MAPS_KENFCT_v1.2.csv'), #that f(x) is to 
+                       row.names = FALSE)   
